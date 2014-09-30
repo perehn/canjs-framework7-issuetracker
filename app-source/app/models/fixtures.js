dcb71a97ecf026bab1b2d0981e8628d1a8ff83c7
perehn <per.ehnbom@gmail.com>
2014-09-30 12:45:18 +0200

    fixtures

--- app-source/app/models/fixtures.js
+++ app-source/app/models/fixtures.js
@@ -19,10 +19,11 @@ function(){
 	
 	
 	var issueStore = initStore([
-	    {title : "Do refactoring", description : 'Need to refactor code base Need to refactor code base Need to refactor code base Need to refactor code base', status : "todo", 
+	    {title : "Look over tests", description : 'We need to look into tests, right now there are none. I guess at least one test wouldnt hurt.', status : "todo", 
 	    	comments : [{date : new Date(), text : 'We should look into this'}, {date : new Date(), text : 'We definitely should'}]},     	          		    
-	    {title : "Look over test", description : 'Need to look over tests', status : "todo"},
-	    {title : "Check header", description : 'The header needs to be checked up', status : "inprogress"}
+	    {title : "Get ready for Iphone and Android", description : 'Use phonegap and publish the app for Iphone and Android', status : "todo"},
+	    {title : "Refactor the code", description : 'We need to refactor the code base', status : "inprogress"},
+	    {title : "Post app on Framework7 forum", description : 'Post comment in the forum', status : "inprogress"}
 	  ]);
 	
 	can.fixture({
